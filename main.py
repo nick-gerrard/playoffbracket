31a94330f8a92df27d066557d8dbe42ad6de3779
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-08 18:38:01 -0400

    add about page, some basic htmx

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from sqlmodel import create_engine, SQLModel
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from models import Team, TeamStats, Series, Season
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from fastapi import Request
 
 engine = create_engine("sqlite:///bracket.db")
-
+templates = Jinja2Templates(directory="templates")
 
 def create_db():
     SQLModel.metadata.create_all(engine)
@@ -22,3 +26,17 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+@app.get("/bracket")
+async def bracket(request: Request):
+    return templates.TemplateResponse(request=request, name="bracket.html")
+
+@app.get("/hello")
+async def hello():
+    return HTMLResponse("<p>Hello world!</p>")
+
+
+@app.get("/about")
+async def about(request: Request):
+    return templates.TemplateResponse(request=request, name="about.html")
+
