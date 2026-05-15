7bb8a86e9780e649f77aa5519de9871084bafb1a
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-15 13:17:16 -0400

    fix missing env vars from conftest

--- conftest.py
+++ conftest.py
@@ -2,4 +2,6 @@ import sys
 import os
 
 os.environ.setdefault("SESSION_SECRET_KEY", "test-secret-key")
+os.environ.setdefault("GOOGLE_CLIENT_ID", "test-client-id")
+os.environ.setdefault("GOOGLE_CLIENT_SECRET", "test-client-secret")
 sys.path.insert(0, os.path.dirname(__file__))
